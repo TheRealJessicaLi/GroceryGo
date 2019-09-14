9df392f0fd9d750c5bd67ddc6fd784c986e3cc28
Jessica Li <jessicabli0225@gmail.com>
2019-09-14 01:30:35 -0400

    edits to layouts

---
+++ app/src/main/java/go/grocery/grocerygo/Onboarding/UserInfoActivity.kt
@@ -0,0 +1,13 @@
+package go.grocery.grocerygo.Onboarding
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import go.grocery.grocerygo.R
+
+class UserInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info)
+    }
+}
---
+++ app/src/main/java/go/grocery/grocerygo/Results/SearchResultsActivity.kt
@@ -0,0 +1,13 @@
+package go.grocery.grocerygo.Results
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import go.grocery.grocerygo.R
+
+class SearchResultsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_results)
+    }
+}
