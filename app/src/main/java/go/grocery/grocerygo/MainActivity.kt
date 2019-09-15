8da2897936c55c96ee8f9b7449d79f320ad2a720
Ethan Zhang <y2788zha@edu.uwaterloo.ca>
2019-09-14 22:05:11 -0400

    bees are dying at an alarming rate

--- app/src/main/java/go/grocery/grocerygo/MainActivity.kt
+++ app/src/main/java/go/grocery/grocerygo/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setSupportActionBar(findViewById(R.id.my_toolbar))
         next_button.setOnClickListener {
             val myIntent = Intent(this@MainActivity, UserInfoActivity::class.java)
             this@MainActivity.startActivity(myIntent)
